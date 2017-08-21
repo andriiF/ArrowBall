621a059f2cdbb9cd6ba1b2aee0b7f92b21a4166d
andriiF <andrii.fedorenko@batusystems.pl>
2017-08-21 17:52:32 +0200

    new mechanic

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -1,124 +1,169 @@
+var flag = 1;
+var field = {
+    width: screen.availWidth * 0.84,
+    height: screen.availHeight * 0.61,
+    draw: function () {
+        //central circle
+        var cir = this.field.getContext('2d');
+        cir.clearRect(0, 0, this.width, this.height);
+        cir.beginPath();
+        cir.arc(this.width / 2 - 2, this.height / 2 - 2, 65, 0, 2 * Math.PI, false);
+        cir.fillStyle = '#62fe8b';
+        cir.fill();
+        cir.lineWidth = 4;
+        cir.strokeStyle = '#fff';
+        cir.stroke();
+        //central small circle
+        var cir = this.field.getContext('2d');
+        cir.beginPath();
+        cir.arc(this.width / 2 - 2, this.height / 2 - 2, 7, 0, 2 * Math.PI, false);
+        cir.fillStyle = '#fff';
+        cir.fill();
+        cir.lineWidth = 10;
+        cir.strokeStyle = '#fff';
+        cir.stroke();
+        //central line
+        var cl = this.field.getContext('2d');
+        cl.strokeStyle = "#fff";
+        cl.lineWidth = 4;
+        cl.beginPath();
+        cl.moveTo(this.width / 2 - 2, 0);
+        cl.lineTo(this.width / 2 - 2, this.height);
+        cl.stroke();
+        //left lines
+        var ll = this.field.getContext('2d');
+        ll.strokeStyle = "#fff";
+        ll.lineWidth = 4;
+        ll.beginPath();
+//left lines
+        ll.moveTo(0, this.height * 0.2);
+        ll.lineTo(this.width * 0.13, this.height * 0.2);
+        ll.lineTo(this.width * 0.13, this.height * 0.8);
+        ll.lineTo(0, this.height * 0.8);
+
+//right lines
+        ll.moveTo(this.width, this.height * 0.2);
+        ll.lineTo(this.width * 0.87, this.height * 0.2);
+        ll.lineTo(this.width * 0.87, this.height * 0.8);
+        ll.lineTo(this.width, this.height * 0.8);
+        ll.stroke();
+
+
+        var lg = this.field.getContext('2d');
+        lg.strokeStyle = "#FF4B4B";
+        lg.lineWidth = 5;
+        lg.beginPath();
+        lg.moveTo(0, this.height * 0.35);
+        lg.lineTo(10, this.height * 0.35);
+        lg.lineTo(10, this.height * 0.65);
+        lg.lineTo(0, this.height * 0.65);
+
+        lg.moveTo(this.width, this.height * 0.35);
+        lg.lineTo(this.width - 10, this.height * 0.35);
+        lg.lineTo(this.width - 10, this.height * 0.65);
+        lg.lineTo(this.width, this.height * 0.65);
+
+        lg.stroke();
+
+
+    }
+}
+
 $(document).ready(function () {
     pos = {};
     var direction = 0;
-    $("#field").on("mousemove", function (e) {
-        pos = {
-            x: event.pageX - this.offsetLeft - 30,
-            y: event.pageY - this.offsetLeft - 45
-        };
-    });
+    var ball = $("#ball");
+    ball.start = function () {
+        this.css({
+            left: field.width / 2 + 18,
+            top: field.height / 2 + 2,
+            opacity: 1
+        });
+    }
+    ball.move = function (a, b, delta) {
+
+        var x = parseInt(this.offset().left.toFixed(0)) + 2000 * delta;
+        var y = parseInt(this.offset().top.toFixed(0));
+
+        var top = a * x + b + y;
+        if (Math.floor(parseInt(top) < 0)) {
+            top = 0;
+        } else if (Math.floor(parseInt(top) > field.height + 15)) {
+            top = field.height + 15;
+        }
+
+        if (x <= 15) { // add condition with top
+            if ((Math.floor(parseInt(top)) > 300 && Math.floor(parseInt(top)) < 350)) {
+                x = 0;
+            } else {
+                x = 15;
+            }
+        } else if (x > parseInt(field.width) + 50) {
+            if ((Math.floor(parseInt(top)) > 300 && Math.floor(parseInt(top)) < 350)) {
+                x = parseInt(field.width) + 35;
+            } else {
+                x = parseInt(field.width) + 27;
+            }
+        }
+
+        ball.animate({
+            left: x.toString(),
+            top: Math.floor(parseInt(top))
+        }, 500);
+
+        flag = 0;
+    }
+    ball.start();
     $("#field").css("margin", ($("body").height() / $("#field").height() / 2));
-    var field = $("#field")[0];
-    field.width = screen.availWidth * 0.8;
-    field.height = screen.availHeight * 0.6;
-    if (field.getContext) {
-        coordinate = {
-            x: 200,
-            y: 200
-        };
+    field.field = $("#field")[0];
+    if (field.field.getContext) {
+        var coordinate = [];
         colors = {
             a: "magenta",
             b: "blue"
         };
-        draw = function () {
-            //central circle
-            var cir = field.getContext('2d');
-            cir.clearRect(0, 0, field.width, field.height);
+        field.draw();
+        ball.bind("mousemove",
+                function (event) {
+                    if (flag == 0) {
+                        coordinate = [];
+                        flag = 1;
+                    }
 
-            cir.beginPath();
-            cir.arc(field.width / 2 - 2, field.height / 2 - 2, 65, 0, 2 * Math.PI, false);
-            cir.fillStyle = '#62fe8b';
-            cir.fill();
-            cir.lineWidth = 4;
-            cir.strokeStyle = '#fff';
-            cir.stroke();
+                    if (coordinate.length < 3) {
+                        temp = {
+                            x: event.clientX,
+                            y: event.clientY
+                        };
 
-            //central small circle
-            var cir = field.getContext('2d');
-            cir.beginPath();
-            cir.arc(field.width / 2 - 2, field.height / 2 - 2, 7, 0, 2 * Math.PI, false);
-            cir.fillStyle = '#fff';
-            cir.fill();
-            cir.lineWidth = 10;
-            cir.strokeStyle = '#fff';
-            cir.stroke();
+                        coordinate.push(temp);
+                    } else if (coordinate.length == 3) {
+                        var a = (coordinate[0].y - coordinate[2].y) / (coordinate[0].x - coordinate[2].x);
+                        var b = (coordinate[0].y - a * coordinate[0].x);
 
-            //central line
-            var cl = field.getContext('2d');
-            cl.strokeStyle = "#fff";
-            cl.lineWidth = 4;
-            cl.beginPath();
-            cl.moveTo(field.width / 2 - 2, 0);
-            cl.lineTo(field.width / 2 - 2, field.height);
-            cl.stroke();
-            //left lines
+                        if ((coordinate[2].x - coordinate[0].x) > 0) {
+                            var delta = 1;
+                        } else {
+                            var delta = -1;
+                        }
+                        ball.move(a.toFixed(2), b.toFixed(2), delta);
+                        setInterval(function () {
+                            if ((ball.offset().left < 75 || ball.offset().left > 1105) && (ball.offset().top > 300 && ball.offset().top < 350)) {
+                                var audio = new Audio('assets/audio/1.mp3');
+                                audio.play();
+                                ball.animate({
+                                    left: field.width / 2 + 18,
+                                    top: field.height / 2 + 2,
+                                }, 5);
+                                coordinate = [];
+                                flag = 0;
+                                return;
+                            }
+                        }, 150);
 
-            var ll = field.getContext('2d');
-            ll.strokeStyle = "#fff";
-            ll.lineWidth = 4;
-            ll.beginPath();
-            ll.moveTo(0, field.height * 0.2);
-            ll.lineTo(field.width * 0.13, field.height * 0.2);
-            ll.lineTo(field.width * 0.13, field.height * 0.8);
-            ll.lineTo(0, field.height * 0.8);
-            ll.moveTo(field.width, field.height * 0.2);
-            ll.lineTo(field.width * 0.87, field.height * 0.2);
-            ll.lineTo(field.width * 0.87, field.height * 0.8);
-            ll.lineTo(field.width, field.height * 0.8);
-            ll.stroke();
-
-            ball = field.getContext('2d');
-            ball.beginPath();
-
-            ball.arc(coordinate.x, coordinate.y, 23, 0, 10 * Math.PI, false);
-            var grd = cir.createRadialGradient(10, 10, 23, 38, 50, 300);
-            grd.addColorStop(0, "black");
-            grd.addColorStop("0.3", colors.a);
-            grd.addColorStop("0.6", colors.b);
-            grd.addColorStop(1, "red");
-            ball.fillStyle = grd;
-            ball.strokeStyle = grd;
-            ball.shadowColor = '#004';
-            ball.shadowBlur = 10;
-            ball.shadowOffsetX = 0;
-            ball.shadowOffsetY = 0;
-            ball.fill();
-            ball.stroke();
-        };
-        draw();
+                    }
+                });
 
 
     }
-    function action() {
-        if (coordinate.x == pos.x) {
-            console.log(pos);
-        }
-
-        if (coordinate.x <= field.width - 30 && coordinate.y <= field.height - 30) {
-            redraw(1);
-        } else {
-            coordinate.x = Math.floor(Math.random() * field.width - 30);
-            coordinate.y = Math.floor(Math.random() * field.height - 30);
-        }
-
-
-
-    }
-
-    setInterval(action, 1);
-
-//    $(document).on("click", function (e) {
-//        var pos = {
-//            x: e.pageX - ((screen.availWidth - screen.availWidth * 0.8) / 2),
-//            y: e.pageY - ((screen.availHeight - screen.availHeight * 0.8) / 2)
-//        };
-//    });
-    redraw = function (speed) {
-        for (var i = 0; i < speed; i++) {
-            coordinate.y++;
-            coordinate.x++;
-        }
-        draw();
-    }
-
 });
